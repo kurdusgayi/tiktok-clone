45467b9be406bf4606db805bbb248e7d3a9a1b9c
kurdusgayi <kjsvsoiw1@gmail.com>
2026-06-28 12:57:16 +0300

    Add signup page

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
-import os
-import uvicorn
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Form
 from fastapi.templating import Jinja2Templates
-from fastapi.staticfiles import StaticFiles
+from fastapi.responses import RedirectResponse
 
 app = FastAPI()
-
 templates = Jinja2Templates(directory="templates")
-app.mount("/static", StaticFiles(directory="static"), name="static")
 
-@app.get("/")
-def read_root(request: Request):
-    return templates.TemplateResponse(request=request, name="index.html")
+# ل ڤێرێ ئه‌م دێ لیسته‌کا داتایان هه‌بیت (بۆ ده‌ستپێکێ)
+users = {}
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port)
+@app.get("/signup")
+def signup_page(request: Request):
+    return templates.TemplateResponse("signup.html", {"request": request})
+
+@app.post("/signup")
+def register(username: str = Form(...), password: str = Form(...)):
+    users[username] = password
+    return RedirectResponse(url="/", status_code=303)
