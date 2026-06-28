f7bc9c0839b863c6070662fdb2481fdc1dc7b49b
kurdusgayi <kjsvsoiw1@gmail.com>
2026-06-28 11:32:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
 from fastapi.templating import Jinja2Templates
-from fastapi.staticfiles import StaticFiles
+from fastapi.requests import Request
 
 app = FastAPI()
-
-# ڤێ په‌لدانکا templatesـێ بۆ HTML فایلان دیار دکه‌ت
 templates = Jinja2Templates(directory="templates")
-
-# ڤێ په‌لدانکا staticـێ بۆ CSS و وێنان دیار دکه‌ت
-app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/")
 def read_root(request: Request):
-    # دێ فایلا index.html نیشان ده‌ت
     return templates.TemplateResponse("index.html", {"request": request})
