f50d27680c9cf4585e1ec4633d5e56548db80a13
kurdusgayi <kjsvsoiw1@gmail.com>
2026-06-28 12:13:57 +0300

    Fix main.py code

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-return templates.TemplateResponse(request=request, name="index.html")
+import os
+import uvicorn
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/")
+def read_root(request: Request):
+    return templates.TemplateResponse(request=request, name="index.html")
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
